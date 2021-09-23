b3a656bb4f66f5f018ebe4f1e86dfce95d53c725
Tuhin Roy <troy0870@gmail.com>
2021-09-23 20:42:38 +0530

    Feat: Create the skeleton for services

--- index.js
+++ index.js
@@ -12,6 +12,44 @@ const app = express();
 const port = 3000;
 
 
+app.get('/', (req, res) => {
+    res.send(
+        `<h1>Takeover your Browser</h1>
+            <p>Available services</p>
+            <p>GET: /start?browser=browser&url=http://example.com</p>
+            <p>GET: /stop?browser=browser</p>
+            <p>GET: /cleanup?browser=chrome</p>
+            <p>GET: /getUrl?browser=chrome</p>
+        `)
+})
+
+/**
+ * Start a browser
+ */
+app.get('/start', (req, res) => {
+    res.send('Run start browser')
+})
+
+/**
+ * Stop a browser
+ */
+app.get('/stop', (req, res) => {
+    res.send('Run stop browser')
+})
+
+/**
+ * Clean a browser history
+ */
+app.get('/cleanup', (req, res) => {
+    res.send('Run clean browser')
+})
+
+/**
+ * Start a browser
+ */
+app.get('/getUrl', (req, res) => {
+    res.send('Run get active url browser')
+})
 
 app.listen(port, () => {
     console.log('Server running at http://localhost:' + port);
