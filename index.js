b3537bc45faddb33f59f79ee47e9d45a8f942abc
Tuhin Roy <troy0870@gmail.com>
2021-09-23 20:16:16 +0530

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * Create an HTTP Server to expose 4 endpoints for browser operations
+ * 
+ * 1. startServer
+ * 2. stopServer
+ * 3. cleanHistory
+ * 4. getCurrentActiveTabForBrowser
+ */
+
+
